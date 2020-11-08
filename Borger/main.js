36f335e38caadea88baef833994f365af93a4a7f
MariusMM2 <dmunteanu91@gmail.com>
2020-11-08 17:17:13 +0100

    added logging

--- Borger/main.js
+++ Borger/main.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const borgerUserRouter = require('./routers/borgerUserRouter');
 const addressRouter = require('./routers/addressRouter');
 const {port} = require('./config');
+const logging = require('morgan');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(logging());
 app.use('/borgerUser', borgerUserRouter);
 app.use('/address', addressRouter);
 
