cadfd425fba9e6d0209b19bf8c0953d8207334c9
MariusMM2 <dmunteanu91@gmail.com>
2020-11-08 13:51:02 +0100

    added CRUD for BorgerUser in Borger API

---
+++ Borger/config.js
@@ -0,0 +1,1 @@
+module.exports.dbLocation = './borger.sqlite';
---
+++ Borger/main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const borgerUserRouter = require('./routers/borgerUserRouter');
+// const addressRouter = require('./routers/addressRouter');
+
+const port = 8001;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/borgerUser', borgerUserRouter);
+// app.use('/address', addressRouter);
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
